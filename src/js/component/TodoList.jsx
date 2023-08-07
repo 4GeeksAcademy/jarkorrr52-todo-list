ba1a360dc12c038fb298e237ed2a2627ba262d82
jarkor5 <67030533+jarkor5@users.noreply.github.com>
2023-08-07 19:22:47 +0000

    second commit

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,28 +1,73 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoItem from './TodoItem';
-import '../../styles/TodoList.css'
+import '../../styles/TodoList.css';
 
 const TodoList = () => {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
 
-  const handleAddTask = () => {
+  useEffect(() => {
+    fetch('https://playground.4geeks.com/apis/fake/todos/user/alesanchezr')
+      .then((response) => response.json())
+      .then((data) => {
+        setTasks(data);
+      })
+      .catch((error) => {
+        console.error('Error al cargar la lista de tareas:', error);
+      });
+  }, []);
+
+  const handleAddTask = async () => {
     if (newTask.trim() !== '') {
-      setTasks([...tasks, newTask]);
+      const newTasks = [...tasks, newTask];
+      setTasks(newTasks);
       setNewTask('');
+
+      try {
+        const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/alesanchezr', {
+          method: 'PUT',
+          body: JSON.stringify(newTasks),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (!response.ok) {
+          console.error('Error al actualizar la lista en el servidor');
+        }
+      } catch (error) {
+        console.error('Error de conexión:', error);
+      }
     }
   };
 
-  const handleDeleteTask = (index) => {
+  const handleDeleteTask = async (index) => {
     const updatedTasks = tasks.filter((_, i) => i !== index);
     setTasks(updatedTasks);
+
+    try {
+      const response = await fetch('https://playground.4geeks.com/apis/fake/todos/user/alesanchezr', {
+        method: 'PUT',
+        body: JSON.stringify(updatedTasks),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        console.error('Error al actualizar la lista en el servidor');
+      }
+    } catch (error) {
+      console.error('Error de conexión:', error);
+    }
   };
 
   return (
     <div className="container">
       <h1>To do List</h1>
       <div className="input-container">
-        <input className='input-text'
+        <input
+          className="input-text"
           type="text"
           value={newTask}
           onChange={(e) => setNewTask(e.target.value)}
